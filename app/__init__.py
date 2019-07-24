546a7ef615983d2173ed6f48bbd31a3e3a19a34a
sc <1131497375@qq.com>
2019-07-24 10:56:40 +0800

    chore：添加了配置文件config.py

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+# coding:utf8
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from config import config
+
+def create_app():
+    app = Flask(__name__)
+    #加载配置文件
+    app.config.from_object(config["development"])
+
+    #注册蓝图
+
+
+    # 初始化mysql
+    # db = SQLAlchemy(app)
+
+    return app
+
+#创建flask应用程序
+app = create_app()
+
+
+
--- manage.py
+++ manage.py
@@ -1,12 +1,5 @@
-from flask import Flask
+# coding:utf8
+from app import app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
